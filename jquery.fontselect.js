a357f4d1e562c810a5cd06288e2ada85247363eb
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-11-25 22:46:11 +0100

    zavrsen css za dizajn

--- jquery.fontselect.js
+++ jquery.fontselect.js
@@ -6,7 +6,7 @@
 
     var settings = {
       style: 'font-select',
-      placeholder: 'Select a font',
+      placeholder: 'Изаберите фонт',
       lookahead: 2,
       api: 'https://fonts.googleapis.com/css?family=',
       fonts: [

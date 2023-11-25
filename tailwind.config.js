488df1d7b06bd1fcace6d50fa06781a58a959e8a
Nikola <pesicnikola50@gmail.com>
2023-11-25 16:22:25 +0100

    popravljenje boje

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,15 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
-    theme: {
+    extend: {
       colors: {
         'text': '#fcfcfd',
         'background': '#061d28',
         'primary': '#ef8e61',
         'secondary': '#092e3e',
         'accent': '#13a4e7',
-       },
-    }
+      },
+    },
   },
   plugins: [],
 }
-

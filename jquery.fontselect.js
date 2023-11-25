cada9fee3833c2f69c57534b025ecda3efc27695
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-11-26 00:42:58 +0100

    api for fonts

--- jquery.fontselect.js
+++ jquery.fontselect.js
@@ -8,237 +8,21 @@
       style: 'font-select',
       placeholder: 'Изаберите фонт',
       lookahead: 2,
-      api: 'https://fonts.googleapis.com/css?family=',
-      fonts: [
-        "Aclonica",
-        "Allan",
-        "Annie+Use+Your+Telescope",
-        "Anonymous+Pro",
-        "Allerta+Stencil",
-        "Allerta",
-        "Amaranth",
-        "Anton",
-        "Architects+Daughter",
-        "Arimo",
-        "Artifika",
-        "Arvo",
-        "Asset",
-        "Astloch",
-        "Bangers",
-        "Bentham",
-        "Bevan",
-        "Bigshot+One",
-        "Bowlby+One",
-        "Bowlby+One+SC",
-        "Brawler",
-        "Buda:300",
-        "Cabin",
-        "Calligraffitti",
-        "Candal",
-        "Cantarell",
-        "Cardo",
-        "Carter One",
-        "Caudex",
-        "Cedarville+Cursive",
-        "Cherry+Cream+Soda",
-        "Chewy",
-        "Coda",
-        "Coming+Soon",
-        "Copse",
-        "Corben:700",
-        "Cousine",
-        "Covered+By+Your+Grace",
-        "Crafty+Girls",
-        "Crimson+Text",
-        "Crushed",
-        "Cuprum",
-        "Damion",
-        "Dancing+Script",
-        "Dawning+of+a+New+Day",
-        "Didact+Gothic",
-        "Droid+Sans",
-        "Droid+Sans+Mono",
-        "Droid+Serif",
-        "EB+Garamond",
-        "Expletus+Sans",
-        "Fontdiner+Swanky",
-        "Forum",
-        "Francois+One",
-        "Geo",
-        "Give+You+Glory",
-        "Goblin+One",
-        "Goudy+Bookletter+1911",
-        "Gravitas+One",
-        "Gruppo",
-        "Hammersmith+One",
-        "Holtwood+One+SC",
-        "Homemade+Apple",
-        "Inconsolata",
-        "Indie+Flower",
-        "IM+Fell+DW+Pica",
-        "IM+Fell+DW+Pica+SC",
-        "IM+Fell+Double+Pica",
-        "IM+Fell+Double+Pica+SC",
-        "IM+Fell+English",
-        "IM+Fell+English+SC",
-        "IM+Fell+French+Canon",
-        "IM+Fell+French+Canon+SC",
-        "IM+Fell+Great+Primer",
-        "IM+Fell+Great+Primer+SC",
-        "Irish+Grover",
-        "Irish+Growler",
-        "Istok+Web",
-        "Josefin+Sans",
-        "Josefin+Slab",
-        "Judson",
-        "Jura",
-        "Jura:500",
-        "Jura:600",
-        "Just+Another+Hand",
-        "Just+Me+Again+Down+Here",
-        "Kameron",
-        "Kenia",
-        "Kranky",
-        "Kreon",
-        "Kristi",
-        "La+Belle+Aurore",
-        "Lato:100",
-        "Lato:100italic",
-        "Lato:300", 
-        "Lato",
-        "Lato:bold",  
-        "Lato:900",
-        "League+Script",
-        "Lekton",  
-        "Limelight",  
-        "Lobster",
-        "Lobster Two",
-        "Lora",
-        "Love+Ya+Like+A+Sister",
-        "Loved+by+the+King",
-        "Luckiest+Guy",
-        "Maiden+Orange",
-        "Mako",
-        "Maven+Pro",
-        "Maven+Pro:500",
-        "Maven+Pro:700",
-        "Maven+Pro:900",
-        "Meddon",
-        "MedievalSharp",
-        "Megrim",
-        "Merriweather",
-        "Metrophobic",
-        "Michroma",
-        "Miltonian Tattoo",
-        "Miltonian",
-        "Modern Antiqua",
-        "Monofett",
-        "Molengo",
-        "Mountains of Christmas",
-        "Muli:300", 
-        "Muli", 
-        "Neucha",
-        "Neuton",
-        "News+Cycle",
-        "Nixie+One",
-        "Nobile",
-        "Nova+Cut",
-        "Nova+Flat",
-        "Nova+Mono",
-        "Nova+Oval",
-        "Nova+Round",
-        "Nova+Script",
-        "Nova+Slim",
-        "Nova+Square",
-        "Nunito:light",
-        "Nunito",
-        "OFL+Sorts+Mill+Goudy+TT",
-        "Old+Standard+TT",
-        "Open+Sans:300",
-        "Open+Sans",
-        "Open+Sans:600",
-        "Open+Sans:800",
-        "Open+Sans+Condensed:300",
-        "Orbitron",
-        "Orbitron:500",
-        "Orbitron:700",
-        "Orbitron:900",
-        "Oswald",
-        "Over+the+Rainbow",
-        "Reenie+Beanie",
-        "Pacifico",
-        "Patrick+Hand",
-        "Paytone+One", 
-        "Permanent+Marker",
-        "Philosopher",
-        "Play",
-        "Playfair+Display",
-        "Podkova",
-        "PT+Sans",
-        "PT+Sans+Narrow",
-        "PT+Sans+Narrow:regular,bold",
-        "PT+Serif",
-        "PT+Serif Caption",
-        "Puritan",
-        "Quattrocento",
-        "Quattrocento+Sans",
-        "Radley",
-        "Raleway:100",
-        "Redressed",
-        "Rock+Salt",
-        "Rokkitt",
-        "Ruslan+Display",
-        "Schoolbell",
-        "Shadows+Into+Light",
-        "Shanti",
-        "Sigmar+One",
-        "Six+Caps",
-        "Slackey",
-        "Smythe",
-        "Sniglet:800",
-        "Special+Elite",
-        "Stardos+Stencil",
-        "Sue+Ellen+Francisco",
-        "Sunshiney",
-        "Swanky+and+Moo+Moo",
-        "Syncopate",
-        "Tangerine",
-        "Tenor+Sans",
-        "Terminal+Dosis+Light",
-        "The+Girl+Next+Door",
-        "Tinos",
-        "Ubuntu",
-        "Ultra",
-        "Unkempt",
-        "UnifrakturCook:bold",
-        "UnifrakturMaguntia",
-        "Varela",
-        "Varela Round",
-        "Vibur",
-        "Vollkorn",
-        "VT323",
-        "Waiting+for+the+Sunrise",
-        "Wallpoet",
-        "Walter+Turncoat",
-        "Wire+One",
-        "Yanone+Kaffeesatz",
-        "Yanone+Kaffeesatz:300",
-        "Yanone+Kaffeesatz:400",
-        "Yanone+Kaffeesatz:700",
-        "Yeseva+One",
-        "Zeyada"
-      ]
+      api: 'https://fonts.googleapis.com/css2?family=',
+      fonts: []      
     };
     
     var Fontselect = (function(){
     
-      function Fontselect(original, o){
+      function Fontselect(original, o) {
         this.$original = $(original);
         this.options = o;
         this.active = false;
-        this.setupHtml();
-        this.getVisibleFonts();
-        this.bindEvents();
+        this.fetchCyrillicFonts().then(() => {
+          this.setupHtml();
+          this.getVisibleFonts();
+          this.bindEvents();
+        });
 
         var font = this.$original.val();
         if (font) {
@@ -246,6 +30,19 @@
           this.addFontLink(font);
         }
       }
+
+      Fontselect.prototype.fetchCyrillicFonts = async function() {
+        try {
+          const response = await fetch('https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBinU4iG2kq02MjO1RZKYyfQNnmrhhPdX8&subset=cyrillic');
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          this.options.fonts = data.items.map(font => font.family);
+        } catch (error) {
+          console.error('Failed to fetch Cyrillic fonts:', error);
+        }
+      };
       
       Fontselect.prototype.bindEvents = function(){
         var self = this;
@@ -330,17 +127,16 @@
         this.$drop.append(this.$results.append(this.fontsAsHtml())).hide();
       };
       
-      Fontselect.prototype.fontsAsHtml = function(){
-        
+      Fontselect.prototype.fontsAsHtml = function() {
         var l = this.options.fonts.length;
         var r, s, h = '';
-        
-        for(var i=0; i<l; i++){
+
+        for (var i = 0; i < l; i++) {
           r = this.toReadable(this.options.fonts[i]);
           s = this.toStyle(this.options.fonts[i]);
-          h += '<li data-value="'+ this.options.fonts[i] +'" style="font-family: '+s['font-family'] +'; font-weight: '+s['font-weight'] +'">'+ r +'</li>';
+          h += '<li data-value="' + this.options.fonts[i] + '" style="font-family: ' + s['font-family'] + '; font-weight: ' + s['font-weight'] + '">' + r + '</li>';
         }
-        
+
         return h;
       };
       
@@ -391,8 +187,7 @@
       return Fontselect;
     })();
 
-    return this.each(function() {        
-      // If options exist, lets merge them
+    return this.each(function() {       
       if (options) $.extend( settings, options );
       
       return new Fontselect(this, settings);

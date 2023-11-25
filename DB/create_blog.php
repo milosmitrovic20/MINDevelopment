78a9a356bc74b19495962edebda6b5de185e1606
Ika1009 <ilija.doncic@yahoo.com>
2023-11-25 18:23:05 +0100

    popravljeni bagovi

--- DB/create_blog.php
+++ DB/create_blog.php
@@ -2,12 +2,12 @@
 header('Content-Type: application/json');
 include 'db_conn.php';
 
-function createBlogPost($pdo, $korisnikId, $naslov, $sadrzaj) {
-    $sql = "INSERT INTO blog (korisnik_id, naslov, sadrzaj) VALUES (?, ?, ?)";
+function createBlogPost($pdo, $korisnikId, $naslov, $sadrzaj, $kategorija) {
+    $sql = "INSERT INTO blog (korisnik_id, naslov, sadrzaj, kategorija) VALUES (?, ?, ?, ?)";
 
     try {
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([$korisnikId, $naslov, $sadrzaj]);
+        $stmt->execute([$korisnikId, $naslov, $sadrzaj, $kategorija]);
         return ["status" => "success", "message" => "Blog post successfully created"];
     } catch (PDOException $e) {
         return ["status" => "error", "message" => $e->getMessage()];
@@ -17,7 +17,12 @@ function createBlogPost($pdo, $korisnikId, $naslov, $sadrzaj) {
 $input = json_decode(file_get_contents('php://input'), true);
 
 if ($input) {
-    $response = createBlogPost($pdo, $input['korisnikId'], $input['naslov'], $input['sadrzaj']);
+    // Ensure that the category is also provided
+    if (!isset($input['kategorija'])) {
+        echo json_encode(["status" => "error", "message" => "Category is required"]);
+        exit;
+    }
+    $response = createBlogPost($pdo, $input['korisnikId'], $input['naslov'], $input['sadrzaj'], $input['kategorija']);
     echo json_encode($response);
 } else {
     echo json_encode(["status" => "error", "message" => "Invalid input"]);
--- magazin/display_blogs.js
+++ magazin/display_blogs.js
@@ -1,5 +1,5 @@
 async function getAllBlogPosts() {
-    const apiUrl = 'http://мајндивелопмент.срб/get_blogs.php'; // Replace with your actual API URL
+    const apiUrl = 'http://мајндивелопмент.срб/DB/get_blogs.php'; // Replace with your actual API URL
 
     try {
         const response = await fetch(apiUrl);
@@ -12,14 +12,27 @@ async function getAllBlogPosts() {
                 const blogElement = `
                     <article class="p-6 bg-secondary rounded-lg border shadow-md border-secondary">
                         <div class="flex justify-between items-center mb-5 text-gray-500">
-                            <!-- Add other elements like date, category here -->
-                            <span class="text-sm">${new Date(blog.datum_objave).toLocaleDateString()}</span>
+                            <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                ${blog.kategorija} <!-- Displaying category -->
+                            </span>
+                            <span class="text-sm">${new Date(blog.datum_objave).toLocaleDateString()}</span> <!-- Displaying date -->
                         </div>
                         <h2 class="mb-2 text-2xl font-bold tracking-tight text-white">
                             <a href="#">${blog.naslov}</a>
                         </h2>
                         <p class="mb-5 font-light text-text">${blog.sadrzaj}</p>
-                        <!-- Add more elements like author, read more link here -->
+                        <div class="flex justify-between items-center">
+                            <div class="flex items-center space-x-4">
+                                <img class="w-7 h-7 rounded-full" src="author_avatar_url.jpg" alt="Author avatar" />
+                                <span class="font-medium text-white">
+                                    Author Name <!-- Replace with actual author name if available -->
+                                </span>
+                            </div>
+                            <a href="#" class="inline-flex items-center font-medium text-primary hover:underline">
+                                Read more
+                                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </a>
+                        </div>
                     </article>
                 `;
                 blogContainer.innerHTML += blogElement;
@@ -33,5 +46,4 @@ async function getAllBlogPosts() {
     }
 }
 
-// Example usage
-document.addEventListener('DOMContentLoaded', getAllBlogPosts);
+getAllBlogPosts();

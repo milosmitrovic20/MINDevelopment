d36b35f293c31ce50a4c647065528e40dd3ee181
Ika1009 <ilija.doncic@yahoo.com>
2023-11-26 12:13:40 +0100

    stavljanje sesije i aploud blogova

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "magazin/magazin.html", "magazin/create_blog.html", "magazin/blog.html", "auth/login.html", "auth/signup.html", "dizajn/dizajn.html"],
+  content: ["index.html", "magazin/magazin.html", "magazin/create_blog.html", "magazin/blog.html", "auth/login.php", "auth/signup.html", "dizajn/dizajn.html"],
   theme: {
     extend: {
       colors: {

b6fe35d5279293adece8123b1636e79374f5e612
Ika1009 <ilija.doncic@yahoo.com>
2023-11-26 23:19:04 +0100

    испис коментара инстантан

--- magazin/blog.js
+++ magazin/blog.js
@@ -34,6 +34,10 @@ async function fetchBlogDetailsAndComments() {
 
                     if (data.status === 'success') {
                         console.log(data.message);
+                        const noviKomentarHTML = createCommentHTML({korisnicko_ime: "Ви", tekst: commentText, datum_komentara: "Сада"});
+                        const commentsContainer = document.querySelector('.commentsSection'); // Adjust selector as needed
+                        commentsContainer.innerHTML = noviKomentarHTML + commentsContainer.innerHTML; // Prepend the new comment
+                        document.getElementById('comment').value = ''; // Clear the input field
                     } else {
                         alert("Дошло је до грешке! " + data.message);
                     }
@@ -90,42 +94,7 @@ function displayBlogDetails(blogDetails, comments, author) {
                     </form>`;
 
     for (let i = 0; i < comments.length; i++) {
-        htmlContent += `
-        <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
-            <footer class="flex justify-between items-center mb-2">
-                <div class="flex items-center">
-                    <p class="inline-flex items-center mr-3 font-semibold text-sm text-gray-900 dark:text-white">${comments[i].korisnicko_ime}</p>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">${comments[i].datum_komentara}</p>
-                </div>
-                <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
-                    class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:text-gray-400 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    type="button">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                        <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
-                    </svg>
-                    <span class="sr-only">Comment settings</span>
-                </button>
-                <div id="dropdownComment1"
-                    class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                    <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                        aria-labelledby="dropdownMenuIconHorizontalButton">
-                        <li>
-                            <a href="#"
-                                class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
-                        </li>
-                    </ul>
-                </div>
-            </footer>
-            <p>${comments[i].tekst}</p>
-        </article>`;
+        htmlContent += createCommentHTML(comments[i]);
     }
 
     htmlContent += `
@@ -137,8 +106,47 @@ function displayBlogDetails(blogDetails, comments, author) {
     blogContainer.innerHTML = htmlContent;
 }
 
+function createCommentHTML(comment) {
+    return `
+        <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
+        <footer class="flex justify-between items-center mb-2">
+            <div class="flex items-center">
+                <p class="inline-flex items-center mr-3 font-semibold text-sm text-gray-900 dark:text-white">${comment.korisnicko_ime}</p>
+                <p class="text-sm text-gray-600 dark:text-gray-400">${comment.datum_komentara}</p>
+            </div>
+            <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
+                class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:text-gray-400 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                type="button">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                    <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+                </svg>
+                <span class="sr-only">Comment settings</span>
+            </button>
+            <div id="dropdownComment1"
+                class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownMenuIconHorizontalButton">
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+        <p>${comment.tekst}</p>
+    </article>`;
+}
+
+
 document.getElementById("subscribeButton").addEventListener("click", async (event) => {
-    console.log("NAJJAACI");
     const emailInput = document.getElementById("email");
     const email = emailInput.value;
     if (!email) { return; }

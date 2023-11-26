78189cff6c267aa687a038b6ef560ab715e005e7
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-11-26 12:41:16 +0100

    logout dugme

--- magazin/magazin.php
+++ magazin/magazin.php
@@ -20,7 +20,9 @@
                     if(isset($_SESSION['userID'])) {
                     ?>
                         <a href="create_blog.html" class="text-secondary bg-primary hover:bg-primary-hover font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Креирај блог</a>
-                        <a href="" class="text-secondary bg-primary hover:bg-primary-hover font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Одјави се</a>
+                        <form action="../DB/logout.php" method="post">
+                            <button type="submit" name="logout" class="uppercase text-text bg-primary font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">Log out</button>
+                        </form>
                     <?php
                     } else {
                     ?>

30076a9f64db1134d2c7ea235dc48784d34a9e74
Ika1009 <ilija.doncic@yahoo.com>
2023-11-26 18:01:00 +0100

    dobijanje korisnickog imena iz komentara

--- DB/get_blog_by_id.php
+++ DB/get_blog_by_id.php
@@ -1,8 +1,10 @@
 <?php
+
+// Dobija i podatke o blogu i komentarima radi manjeg API zvanja i brzeg koriscenja
+
 header('Content-Type: application/json');
 include 'db_conn.php';
 
-// Get the blogId from the query string
 $blogId = isset($_GET['blogId']) ? $_GET['blogId'] : null;
 
 if (!$blogId) {
@@ -11,7 +13,6 @@ if (!$blogId) {
 }
 
 try {
-    // Fetch the blog details
     $blogSql = "SELECT * FROM blog WHERE blog_id = ?";
     $blogStmt = $pdo->prepare($blogSql);
     $blogStmt->execute([$blogId]);
@@ -22,13 +23,15 @@ try {
         exit;
     }
 
-    // Fetch the comments for the blog
-    $commentSql = "SELECT * FROM komentari WHERE blog_id = ? ORDER BY datum_komentara DESC";
+    $commentSql = "SELECT komentari.komentar_id, komentari.tekst, komentari.datum_komentara, korisnici.korisnicko_ime
+                    FROM komentari
+                    INNER JOIN korisnici ON komentari.korisnik_id = korisnici.korisnik_id
+                    WHERE komentari.blog_id = ?
+                    ORDER BY komentari.datum_komentara DESC";
     $commentStmt = $pdo->prepare($commentSql);
     $commentStmt->execute([$blogId]);
     $comments = $commentStmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Combine blog details and comments in the response
     $responseData = [
         "blogDetails" => $blog,
         "comments" => $comments

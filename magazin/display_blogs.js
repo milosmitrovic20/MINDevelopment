28eab333c98c05c330e07e02f1afde8daec40667
Ika1009 <ilija.doncic@yahoo.com>
2023-11-26 21:52:10 +0100

    бољи испис блогова и динамични

--- magazin/display_blogs.js
+++ magazin/display_blogs.js
@@ -41,64 +41,46 @@ async function getAllBlogPosts() {
             const blogContainer = document.querySelector('.grid'); // Assuming this is your grid container
 
             data.data.forEach(blog => {
-                const blogElement = `
-                    <article class="p-6 bg-secondary rounded-lg border shadow-md border-secondary">
-                        <div class="flex justify-between items-center mb-5 text-gray-500">
-                            <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                                ${blog.kategorija} <!-- Displaying category -->
-                            </span>
-                            <span class="text-sm">${new Date(blog.datum_objave).toLocaleDateString()}</span> <!-- Displaying date -->
-                        </div>
-                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-white">
-                            <a href="#">${blog.naslov}</a>
-                        </h2>
-                        <p class="line-clamp-3 mb-5 font-light text-text">${blog.sadrzaj}</p>
-                        <div class="flex justify-between items-center">
-                            <div class="flex items-center space-x-4">
-                                <img class="w-7 h-7 rounded-full" src="author_avatar_url.jpg" alt="Author avatar" />
-                                <span class="font-medium text-white">
-                                    Author Name <!-- Replace with actual author name if available -->
-                                </span>
-                            </div>
-                            <a href="blog.html?blogId=${blog.blog_id}" class="inline-flex items-center font-medium text-primary hover:underline">
-                                Read more
-                                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </a>
-                        </div>
-                    </article>
-                `;
+                const blogElement = createBlogPostHTML(blog);
                 blogContainer.innerHTML += blogElement;
             });
         } else {
-            console.error('Failed to fetch blogs:', data.message);
-            // Handle the error case
+            alert(data.message);
         }
     } catch (error) {
-        console.error('Error:', error); // Handle network or other errors
+        alert("Дошло је до непознате грешке");
     }
 }
 
-getAllBlogPosts();
-
 function createBlogPostHTML(blog) {
-    const currentDate = new Date().toLocaleDateString();
-    
     return `
         <article class="p-6 bg-secondary rounded-lg border shadow-md border-secondary">
             <div class="flex justify-between items-center mb-5 text-gray-500">
                 <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                     ${blog.kategorija} <!-- Displaying category -->
                 </span>
-                <span class="text-sm">${currentDate}</span> <!-- Displaying current date -->
+                <span class="text-sm">${new Date(blog.datum_objave).toLocaleDateString()}</span> <!-- Displaying current date -->
             </div>
             <h2 class="mb-2 text-2xl font-bold tracking-tight text-white">
                 <a href="#">${blog.naslov}</a> <!-- Displaying title -->
             </h2>
             <p class="line-clamp-3 mb-5 font-light text-text">${blog.sadrzaj}</p> <!-- Displaying content -->
-            <!-- Additional blog details here (e.g., author name, read more link) -->
+            <div class="flex justify-between items-center">
+                <div class="flex items-center space-x-4">
+                    <img class="w-7 h-7 rounded-full" src="../images/ikone/${blog.autor_slika}.png" alt="Author avatar" />
+                    <span class="font-medium text-white"> ${blog.autor_korisnicko_ime} </span>
+                </div>
+                <a href="blog.html?blogId=${blog.blog_id}" class="inline-flex items-center font-medium text-primary hover:underline">
+                    Read more
+                    <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
         </article>
     `;
 }
+
+getAllBlogPosts();
+
 
 document.getElementById('blogForm').addEventListener('submit', async function(event) {
     event.preventDefault();

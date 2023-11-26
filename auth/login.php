e89abc9609e76451ff274ac73617253f274fa5c1
Ika1009 <ilija.doncic@yahoo.com>
2023-11-26 13:42:17 +0100

    radi login

--- auth/login.php
+++ auth/login.php
@@ -29,14 +29,14 @@ if (isset($_SESSION['userID'])) {
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-text md:text-2xl">
                         Пријави се
                     </h1>
-                    <form action="#">
+                    <form>
                         <div class="mb-4">
-                            <label for="email" class="block mb-2 text-sm font-medium text-text">Корисничко име или мејл</label>
-                            <input required name="username" id="username" class="text-text sm:text-sm rounded-lg block w-full p-2.5 bg-secondary placeholder-gray-400" placeholder="Петар Петровић" required="">
+                            <label for="username" class="block mb-2 text-sm font-medium text-text">Корисничко име или мејл</label>
+                            <input required type="text" name="username" id="username" class="text-text sm:text-sm rounded-lg block w-full p-2.5 bg-secondary placeholder-gray-400" placeholder="Петар Петровић">
                         </div>
                         <div class="lg: mb-2">
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Лозинка</label>
-                            <input required type="password" name="password" id="password" placeholder="••••••••" class="text-text sm:text-sm rounded-lg block w-full p-2.5 bg-secondary placeholder-gray-400" required="">
+                            <input required type="password" name="password" id="password" class="text-text sm:text-sm rounded-lg block w-full p-2.5 bg-secondary placeholder-gray-400" placeholder="••••••••">
                         </div>
                         <div class="flex items-end justify-end mb-8">
                             <a href="#" class="text-sm font-medium text-primary hover:underline">Заборављена лозинка?</a>
@@ -46,6 +46,7 @@ if (isset($_SESSION['userID'])) {
                             Немате налог? <a href="./signup.html" class="font-medium text-primary hover:underline">Региструј се</a>
                         </p>
                     </form>
+
                 </div>
             </div>
         </div>

91e5c98b8836cdd3597a626128126f2e48beb671
Ika1009 <ilija.doncic@yahoo.com>
2023-11-26 17:47:55 +0100

    fixovan milosev bag

--- magazin/blog.js
+++ magazin/blog.js
@@ -32,7 +32,7 @@ function displayBlogDetails(blogDetails, comments) {
     const blogContainer = document.querySelector('#blogContainer');
 
     // Start with the main HTML structure
-    const htmlContent = `
+    let htmlContent = `
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">

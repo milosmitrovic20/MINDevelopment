bbb36f4d2037799f181766f20f6a83e2d06858ac
Ika1009 <ilija.doncic@yahoo.com>
2023-11-26 20:43:01 +0100

    slike su dobre

--- magazin/blog.js
+++ magazin/blog.js
@@ -67,7 +67,7 @@ function displayBlogDetails(blogDetails, comments, author) {
                 <header class="mb-4 lg:mb-6 not-format">
                     <address class="flex items-center mb-6 not-italic">
                         <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                            <img class="mr-4 w-16 h-16 rounded-full" src="../images/${author.slika}.png" alt="Jese Leos">
+                            <img class="mr-4 w-16 h-16 rounded-full" src="../images/ikone/${author.slika}.png" alt="Jese Leos">
                             <div>
                                 <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">${author.korisnicko_ime}</a>
                                 <p class="text-base text-gray-500 dark:text-gray-400">${author.ime} ${author.prezime}</p>
